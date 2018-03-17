86f65b439b01b474e7ee1b9e8f4e9ab819bbe848
JarvisSu <Jarvis_su@126.com>
2018-03-17 17:04:31 +0800

    Initial commit

---
+++ CleanMyMacFiles/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CleanMyMacFiles
+//
+//  Created by Jarvis Su on 17/03/2018.
+//  Copyright © 2018 Jarvis Su. All rights reserved.
+//
+
+import Cocoa
+
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+    }
+
+
+    override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+    }
+
+
+
+}
